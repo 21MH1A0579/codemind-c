03bd7a59589b09fadca5647c0859bdf36fa80e2d
chalumuri brahmendra <97449558+21MH1A0579@users.noreply.github.com>
2022-05-28 01:09:36 +0530

    #include<stdio.h> int prime(int n) {     int i,c=0;     if(n==1)     {         return 0;     }     for(i=2;i<(n/2)+1;i++)     {         if(n%i==0)         {             c=0;             return 0;         }         else         {            c=1;         }     }     if(c==0)     {         return 1;     }     return 1; }
    int main()
    {
        int n,arr[100],i,k,res,c=0;
        scanf("%d",&n);
        for(i=0;i<n;i++)
        {
            scanf("%d",&arr[i]);
        }
        scanf("%d",&k);
        for(i=0;i<n;i++)
        {
            if(prime(arr[i]))
            {
            		c++;
    		}
        }
        printf("%d",c);
    }

---
+++ Primes_in_the_array.c
@@ -0,0 +1,45 @@
+#include<stdio.h>
+int prime(int n)
+{
+    int i,c=0;
+    if(n==1)
+    {
+        return 0;
+    }
+    for(i=2;i<(n/2)+1;i++)
+    {
+        if(n%i==0)
+        {
+            c=0;
+            return 0;
+        }
+        else
+        {
+           c=1;
+        }
+    }
+    if(c==0)
+    {
+        return 1;
+    }
+    return 1;
+}
+
+int main()
+{
+    int n,arr[100],i,k,res,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d",&k);
+    for(i=0;i<n;i++)
+    {
+        if(prime(arr[i]))
+        {
+        		c++;
+		}
+    }
+    printf("%d",c);
+}
