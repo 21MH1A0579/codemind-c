f1c7b21699ab6b8b90e650428941acaa28314004
chalumuri brahmendra <97449558+21MH1A0579@users.noreply.github.com>
2022-05-27 20:03:54 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,min=99,c=0,dc=0,cc=0,temp,temp1;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         c=0;         temp=arr[i];         while(temp)         {             c++;             temp=temp/10;         }         if(c<min)         {             min=c;         }     }     for(i=0;i<n;i++)     {         dc=0;         temp1=arr[i];         while(temp1)         {             dc++;             temp1=temp1/10;         }         if(dc==min)         {             cc++;         }     }     printf("%d",cc); }

---
+++ Minimum_digit_elements.c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,min=99,c=0,dc=0,cc=0,temp,temp1;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        c=0;
+        temp=arr[i];
+        while(temp)
+        {
+            c++;
+            temp=temp/10;
+        }
+        if(c<min)
+        {
+            min=c;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        dc=0;
+        temp1=arr[i];
+        while(temp1)
+        {
+            dc++;
+            temp1=temp1/10;
+        }
+        if(dc==min)
+        {
+            cc++;
+        }
+    }
+    printf("%d",cc);
+}
