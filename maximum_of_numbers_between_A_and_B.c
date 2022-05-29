acf225a216427031dbb389e2f8441993e2c42250
chalumuri brahmendra <97449558+21MH1A0579@users.noreply.github.com>
2022-05-29 15:26:58 +0530

    #include<stdio.h> int main() {     int n,i,arr[100],a,b,j,sum=0,max=0,c=0;     scanf("%d",&n);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     scanf("%d%d",&a,&b);     for(i=0;i<n;i++)     {         for(j=a;j<=b;j++)         {             if(arr[i]==j)             {                if(arr[i]>max)                {                    c=1;                    max=arr[i];                }             }         }     }     if(c==1)     {     printf("%d",max);     }     else     {         printf("-1");     } }

---
+++ maximum_of_numbers_between_A_and_B.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,arr[100],a,b,j,sum=0,max=0,c=0;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    scanf("%d%d",&a,&b);
+    for(i=0;i<n;i++)
+    {
+        for(j=a;j<=b;j++)
+        {
+            if(arr[i]==j)
+            {
+               if(arr[i]>max)
+               {
+                   c=1;
+                   max=arr[i];
+               }
+            }
+        }
+    }
+    if(c==1)
+    {
+    printf("%d",max);
+    }
+    else
+    {
+        printf("-1");
+    }
+}
