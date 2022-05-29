571de609b0daad73759068f0d9d4929020ce27ca
chalumuri brahmendra <97449558+21MH1A0579@users.noreply.github.com>
2022-05-29 16:08:21 +0530

    #include<stdio.h> int main() {     int n,arr[100],i,x,c=0;     scanf("%d%d",&n,&x);     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%x==0)         {             c++;         }     }     printf("%d",c);
    }

---
+++ Count_elements_divisible_by_k.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,arr[100],i,x,c=0;
+    scanf("%d%d",&n,&x);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%x==0)
+        {
+            c++;
+        }
+    }
+    printf("%d",c);
+    
+}
