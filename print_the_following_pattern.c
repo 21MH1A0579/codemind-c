deeb709ca6a72ac6b6639f6edca3241d45264ad0
chalumuri brahmendra <97449558+21MH1A0579@users.noreply.github.com>
2022-05-29 12:02:48 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     for(i=1;i<=n;i++)     {         for(j=1;j<=n;j++)         {             if(i==j)             {                 printf("0");             }             else             {                 printf("x");             }         }         printf(" ");     } }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,17 +1,21 @@
 #include<stdio.h>
 int main()
 {
-    int n,i,j,arr[100];
-    int ch;
+    int n,i,j;
     scanf("%d",&n);
-    ch=65+n-1;
     for(i=1;i<=n;i++)
     {
-        for(j=n;j>=i;j--)
+        for(j=1;j<=n;j++)
         {
-            printf("%c ",ch);
+            if(i==j)
+            {
+                printf("0");
+            }
+            else
+            {
+                printf("x");
+            }
         }
-        ch--;
         printf("
 ");
     }
