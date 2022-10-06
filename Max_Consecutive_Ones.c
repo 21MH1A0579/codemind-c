f8e695f4299762cdb8583a3bdd92966b2492eef5
chalumuri brahmendra <97449558+21MH1A0579@users.noreply.github.com>
2022-10-07 00:01:03 +0530

    #include<stdio.h> int Consec(int* nums, int numsSize) {  int max = 0;  int sum = 0;  for (int i=0; i<numsSize; i++)  {      sum = (sum+nums[i])*nums[i];      if(max<sum)      {          max=sum;
         }
     }
    return max;
    }
    int main()
    {
        int n,i;
        scanf("%d",&n);
        int a[n];
        for(i=0;i<n;i++)
        {
            scanf("%d",&a[i]);
        }
        printf("%d",Consec(a,n));
    }

---
+++ Max_Consecutive_Ones.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int Consec(int* nums, int numsSize)
+{
+ int max = 0;
+ int sum = 0;
+ for (int i=0; i<numsSize; i++)
+ {
+     sum = (sum+nums[i])*nums[i];
+     if(max<sum)
+     {
+         max=sum;
+         
+     }
+ }
+return max;
+}
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("%d",Consec(a,n));
+}
